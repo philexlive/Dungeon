61f9f9a0a40fa952222e2a932fd171f94e1c48dd
philex <philex.live@gmail.com>
2025-08-10 14:57:51 +0300

    Viewport implemented

--- lib/camera.py
+++ lib/camera.py
@@ -1,16 +1,47 @@
 from lib.render import draw
 
 
-def viewport(position, size, *args):
-    x0, y0 = position
-    x1, y1 = size
-    
-    for y in range(y0, y1):
-        for x in range(x0, x1):
-            draw(x, y, ' ')
+class Obj:
+    def __init__(self, pos, mesh):
+        self.x, self.y = pos
+        self.mesh = mesh
 
+obj1 = Obj(
+    (2, 2),
+    [
+        ['*', '.', '.', '*'],
+        ['.', '.', '*', '.'],
+        ['.', '*', '.', '.'],
+        ['*', '.', '.', '*'],
+    ]
+)
+
+
+def obj_to_primitives(obj):
+    primitives = []
+    x_len, y_len = (len(obj.mesh), len(obj.mesh[0])) if obj.mesh else (0, 0)
+    for y in range(y_len):
+        for x in range(x_len):
+            
+            primitives.append((x+obj.x, y+obj.y, obj.mesh[y][x]))
+    return primitives
+
+
+
+def draw_viewport(pos, size, *objs):
+    primitives = []
+
+    for obj in objs:
+        primitives += obj_to_primitives(obj)
+
+    draw_within(pos[0], pos[1], size[0], size[1], *primitives)
+        
+
+
+def draw_within(x0, y0, x1, y1, *args):
     for arg in args:
-        is_within_x = arg[0][0] in range(x0, x1)
-        is_within_y = arg[0][1] in range(y0, y1)
-        if is_within_x and is_within_y:
-            draw(arg[0][0], arg[0][1], arg[1])
+        x_within = arg[0] in range(x0, x1)
+        y_within = arg[1] in range(y0, y1)
+
+        if x_within and y_within:
+            draw(arg[0], arg[1], arg[2])
