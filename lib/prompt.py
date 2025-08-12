749c8f5a9a0fdab4d6f4e41a69669dc0e72e37b6
philex <philex.live@gmail.com>
2025-08-13 00:26:52 +0300

    Prompt documented

--- lib/prompt.py
+++ lib/prompt.py
@@ -5,7 +5,15 @@ class PromptState:
 _prompt = PromptState("Y->==>")
 
 def change_prompt(new):
+    """Change prompt style.
+
+    :param new: str - a new prompt pattern.
+
+    Saves prompt style in global prompt's state.
+    """
     _prompt.style = new
 
 def action():
+    """Get input with stylized prompt.
+    """
     return input(_prompt.style)
